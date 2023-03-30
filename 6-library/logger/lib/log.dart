a9fbadaa21fa1fe2a1de81969126a12f5bb63325
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-03-30 11:46:32 -0300

    tornado propriedades privadas

--- 6-library/logger/lib/log.dart
+++ 6-library/logger/lib/log.dart
@@ -2,23 +2,25 @@ library logger;
 
 import 'dart:io' as io;
 
-enum CoresANSI { verde, vermelho, azul }
+enum _CoresANSI { verde, vermelho, azul }
 
-const resetarCor = '\x1b[m';
+enum Nivel { info, warning, error }
+
+const _resetarCor = '\x1b[m';
 
 // Tabela com cores: https://talyian.github.io/ansicolors/
-const ansiCores = {
-  CoresANSI.vermelho: '\x1b[31m',
-  CoresANSI.verde: '\x1b[32m',
-  CoresANSI.azul: '\x1b[36m',
+const _ansiCores = {
+  _CoresANSI.vermelho: '\x1b[31m',
+  _CoresANSI.verde: '\x1b[32m',
+  _CoresANSI.azul: '\x1b[36m',
 };
 
-void info(Object object) => log(CoresANSI.verde, '[ERROR] $object');
-void warning(Object object) => log(CoresANSI.azul, '[ERROR] $object');
-void error(Object object) => log(CoresANSI.vermelho, '[ERROR] $object');
+void info(Object object) => _log(_CoresANSI.verde, '[INFO] $object');
+void warning(Object object) => _log(_CoresANSI.azul, '[WARNING] $object');
+void error(Object object) => _log(_CoresANSI.vermelho, '[ERROR] $object');
 
-void log(CoresANSI cor, Object object) {
-  io.stdout.writeln('${ansiCores[cor]}$object$resetarCor');
+void _log(_CoresANSI cor, Object object) {
+  io.stdout.writeln('${_ansiCores[cor]}$object$_resetarCor');
 }
 
 // o trecho '\x1b[31m' define a cor vermelha
